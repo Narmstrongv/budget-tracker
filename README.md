4b9cd33f856918662b09531667efcdd1f3184900
Newton <narmstrongv@gmail.com>
2021-08-29 15:05:46 -0700

    added screenshot/updating idb

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 The budget tracker allows the user to add expenses and deposits to their budget with or without an online connection.
 
 ## Screenshot
+![Budget Tracker Screenshot](public/icons/budget_track.png)
 
 ## Website
 https://dry-wave-04051.herokuapp.com/
--- public/js/idb.js
+++ public/js/idb.js
@@ -5,4 +5,23 @@ const request = indexedDB.open('budget_tracker', 1);
 //this event will emit if db ver changes (nonexistant to ver 1/2/3 etc)
 request.onupgradeneeded = function(event) {
     //save a ref to db
-}
+    const db = event.target.result;
+    //create an obj store(table) called 'new_budget', set it to have an auto incrementing primary key of sorts
+    db.createObjectStore('new_budget', { autoIncrement: true }); 
+};
+
+//upon successful...
+request.onsuccess = function(event) {
+    //when db is succesfully created w/its object store (from onupgradedneeded event above) or made a connect, save ref to db in global vari
+    db = event.target.result;
+
+    //check if app online, if Y run uploadBudget() to send all local db data to api
+    if (navigator.onLine) {
+        //uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    //log error
+    console.log(event.target.errorCode);
+};
